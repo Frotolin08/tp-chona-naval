c66aeeb27981ac37881ea2ffc19d74dde29afbf8
Skuerjuan <maierowiczfelipe@gmail.com>
2025-04-18 23:40:37 -0300

    listo

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def creacionBarco(sentido: bool, tablero: list[list[int]], size: int, x: int, y:
                 tablero[x+j][y] = i
             else:  break
 
-def disparo(tablero: list[list[int]], lista_disparados: list[bool], disparos_acertados: int):
+def disparo(tablero: list[list[int]], disparos_acertados: int):
     coords: str = input() # Ingreso de coords del jugador
 
     if (len(coords) < 3): # viendo que tenga el tamaño correcto https://www.w3schools.com/python/gloss_python_string_length.asp
@@ -46,19 +46,13 @@ def disparo(tablero: list[list[int]], lista_disparados: list[bool], disparos_ace
         print("Input inválido.")
         return
 
-    if (tablero[x][y] != 0 and not lista_disparados[tablero[x][y]]): # Hay un barco en esas coords y todavía no lo hundieron
-        lista_disparados[tablero[x][y]] = True # registrando que ese barco fue hundido
+    if (tablero[x][y] != 0): # Hay un barco en esas coords y todavía no lo hundieron
+        tablero[x][y] = 0 # registrando que ese barco fue hundido
         disparos_acertados += 1
         print("El disparo acertó a uno de los barcos.")
 
     else:
         print("El disparo no acertó.")
-
-def actualizarTablero(tablero: list[list[int]], lista_disparados: list[bool]): # para la versión final de los tableros al tener en cuenta todos los barcos hundidos
-    for i in range(N):
-        for j in range(N):
-            if(lista_disparados[tablero[i][j]]):
-                tablero[i][j] = 0
 
 multiplayer: bool = bool(int(input("Singleplayer (0) o Multiplayer (1)?\n"))) # definiendo modo de juego
 
@@ -66,7 +60,6 @@ if (not multiplayer): # sección singleplayer
 
     # declarando tablero en términos de N
     tablero: list[list[int]] = [[0 for _ in range(N)] for _ in range(N)]
-    lista_disparados: list[bool] = [False for _ in range(NUM_BARCOS+1)]
 
     # generación aleatoria de barcos
     for i in range(1, NUM_BARCOS+1):
@@ -78,18 +71,11 @@ if (not multiplayer): # sección singleplayer
 
         creacionBarco(sentido, tablero, size, x, y)
 
-
     for i in range(NUM_DISPAROS): # iterando sobre cada disparo posible
         print("Ingresar coordenadas para disparar (en formato 'y x').\n")
-        disparo(tablero, lista_disparados, disparos_acertados)
+        disparo(tablero, disparos_acertados)
 
     print(f"\nJuego finalizado. Se acertaron {disparos_acertados} tiros y se fallaron {NUM_DISPAROS - disparos_acertados}.")
-
-    print("\nTablero inicial:")
-    for i in tablero:
-        print(i)
-
-    actualizarTablero(tablero, lista_disparados)
 
     print("\nTablero final: ")
 
@@ -106,8 +92,6 @@ else:
     # declarando tablero en términos de N
     tablero: list[list[int]] = [[0 for _ in range(N)] for _ in range(N)]
     tablero_opp: list[list[int]] = [[0 for _ in range(N)] for _ in range(N)]
-    lista_disparados: list[bool] = [False for _ in range(NUM_BARCOS+1)]
-    lista_disparados_opp: list[bool] = [False for _ in range(NUM_BARCOS+1)]
 
     disparos_acertados_opp: int = 0
 
@@ -116,7 +100,6 @@ else:
 
     for i in range(1, NUM_BARCOS+1):
 
-        
         barco: str = input() # recolección de todos los datos
         datos: list[int] = barco.split()
         x: int = int(datos[0])
@@ -159,30 +142,18 @@ else:
     print(f"\nJugador 1: Tenés {NUM_DISPAROS} para derribar la máxima cantidad de barcos de Jugador 2.")
     print("Ingresar coordenadas para disparar (en formato 'x y').\n")
     for i in range(NUM_DISPAROS):
-        disparo(tablero_opp, lista_disparados_opp, disparos_acertados)
+        disparo(tablero_opp, disparos_acertados)
 
     print(f"\nJugador 2: Tenés {NUM_DISPAROS} para derribar la máxima cantidad de barcos de Jugador 1.")
     print("Ingresar coordenadas para disparar (en formato 'x y').\n")
     for i in range(NUM_DISPAROS):
-        disparo(tablero, lista_disparados, disparos_acertados_opp)
+        disparo(tablero, disparos_acertados_opp)
     
     print(f"Juego finalizado. Jugador 1 acertó {disparos_acertados} tiros y Jugador 2 acertó {disparos_acertados_opp}. ")
-
-    print("\nTablero inicial J1:")
-    for i in tablero:
-        print(i)
-    
-    actualizarTablero(tablero, lista_disparados)
 
     print("\nTablero final J1:")
     for i in tablero:
         print(i)
-
-    print("\nTablero inicial J2:")
-    for i in tablero_opp:
-        print(i)
-    
-    actualizarTablero(tablero_opp, lista_disparados_opp)
 
     print("\nTablero final J2:")
     for i in tablero_opp:
